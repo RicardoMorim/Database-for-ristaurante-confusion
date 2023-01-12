a2dcff52be03f1995589c688c7abd817d5935492
RicardoMorim <ricardomorim05@gmail.com>
2023-01-12 19:37:50 +0000

    Error corrections

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
-// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-
-server.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
-
 const server = jsonServer.create()
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
 server.use(middlewares)
-// Add this before server.use(router)
 server.use(jsonServer.rewriter({
     '/api/*': '/$1',
     '/blog/:resource/:id/show': '/:resource/:id'
@@ -23,5 +13,4 @@ server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Export the Server API
 module.exports = server
